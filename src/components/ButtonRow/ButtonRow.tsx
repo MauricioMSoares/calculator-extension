a24c3beb1f2f24a545f34f9b6a03cd990d80298d
Mauricio Soares <mauricio.massayuki@gmail.com>
2025-05-09 17:00:11 -0300

    feat: started creation of ButtonRow component.

---
+++ src/components/ButtonRow/ButtonRow.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface ButtonRowProps {
+    amount: number
+    labels: string[]
+    actions: Array<(label: string) => void>
+}
+
+const ButtonRow = ({amount, labels, actions}: ButtonRowProps) => {
+    return <>
+        
+    </>
+}
+
+export default ButtonRow
